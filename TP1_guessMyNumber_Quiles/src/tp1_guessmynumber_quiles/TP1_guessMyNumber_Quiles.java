f9b78a7f47dde2501c04f77f1d896e2e37a31af5
camsq <camsq@10.3.62.91>
2025-10-24 12:05:47 +0200

    suite TP

--- TP1_guessMyNumber_Quiles/src/tp1_guessmynumber_quiles/TP1_guessMyNumber_Quiles.java
+++ TP1_guessMyNumber_Quiles/src/tp1_guessmynumber_quiles/TP1_guessMyNumber_Quiles.java
@@ -4,6 +4,7 @@
  */
 package tp1_guessmynumber_quiles;
 
+import java.util.Random;
 import java.util.Scanner;
 
 /**
@@ -15,12 +16,28 @@ public class TP1_guessMyNumber_Quiles {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        Random generateurAleat = new Random(); 
-        int n = generateurAleat.nextInt(100);
-        System.out.println("Essaye...");
-        Scanner sc = new Scanner(System.in);
-        int nbr=sc.nextInt(); 
+public static void main(String[] args) {
+    Random generateurAleat = new Random(); 
+    int n = generateurAleat.nextInt(100);
+    System.out.println("Essaye...");
+    Scanner sc = new Scanner(System.in);
+    int nbr=sc.nextInt();
+    int comp=1;
+    while (n!=nbr) {
+    comp +=1;
+    if (n>nbr){
+        System.out.println("trop petit");
+        System.out.println("Essaye encore...");
+        nbr=sc.nextInt();
+    }  
+    else {
+        System.out.println("Trop grand ");
+        System.out.println("Essaye encore...");
+        nbr=sc.nextInt();
+    }
+    }
+    System.out.println("TROUVERRRR");
+    System.out.println("Tentative="+comp);
     }
     
 }
