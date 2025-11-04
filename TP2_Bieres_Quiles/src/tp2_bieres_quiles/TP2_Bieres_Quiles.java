e16bfafbdd0245475ca42d2e800349bbfc8ed543
camsq <camsq@10.3.62.91>
2025-11-04 11:37:26 +0100

    Fin TP

--- TP2_Bieres_Quiles/src/tp2_bieres_quiles/TP2_Bieres_Quiles.java
+++ TP2_Bieres_Quiles/src/tp2_bieres_quiles/TP2_Bieres_Quiles.java
@@ -18,18 +18,27 @@ public class TP2_Bieres_Quiles {
         uneBiere.lireEtiquette();
         
         BouteilleBiere deuxBiere = new BouteilleBiere("Leffe", 6.6 ,"Abbaye de Leffe") ; 
+        deuxBiere.ouverte = false;
         deuxBiere.lireEtiquette();
+        deuxBiere.Decapsuler();
         
         BouteilleBiere troisBiere = new BouteilleBiere("Heineken", 5.0, "Heineken International") ; 
         troisBiere.lireEtiquette();
+        troisBiere.Decapsuler();
+       
         
         BouteilleBiere quatreBiere = new BouteilleBiere("Chimay Bleue", 9.0, "Abbaye de Scourmont") ; 
         quatreBiere.lireEtiquette();
         
         BouteilleBiere cinqBiere = new BouteilleBiere("Karmeliet Triple", 8.4, "Bosteels") ; 
         cinqBiere.lireEtiquette();
+        cinqBiere.Decapsuler();
         
-    System.out.println(uneBiere);
+        System.out.println(uneBiere);
+        System.out.println(deuxBiere);
+        System.out.println(troisBiere);
+        System.out.println(quatreBiere);
+        System.out.println(cinqBiere);
     }
     
     
