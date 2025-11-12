b2823b9e7b514a345a521b017691d407140a6822
camsq <camsq@10.3.62.91>
2025-11-12 11:15:29 +0100

    TP probleme

---
+++ TP3_Heroic_Fantasy_Quiles/src/tp3_heroic_fantasy_quiles/Arme.java
@@ -0,0 +1,33 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package tp3_heroic_fantasy_quiles;
+
+/**
+ *
+ * @author camsq
+ */
+public class Arme {
+    String nom;
+    int niv;
+    
+    public Arme(String nom, int niv) {
+        this.nom = nom;
+        if (niv <= 100)
+            this.niv = niv;
+        else
+            this.niv = 100;  
+    }
+    public int Getniv() {
+        return niv;
+    }
+    public String getNom() {
+        return nom;
+    }
+     @Override
+    public String toString() {
+        return "Arme : " + nom + " | Niveau d’attaque : " + niv;
+    }
+    
+}
---
+++ TP3_Heroic_Fantasy_Quiles/src/tp3_heroic_fantasy_quiles/TP3_Heroic_Fantasy_Quiles.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tp3_heroic_fantasy_quiles;
+
+/**
+ *
+ * @author camsq
+ */
+public class TP3_Heroic_Fantasy_Quiles {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
