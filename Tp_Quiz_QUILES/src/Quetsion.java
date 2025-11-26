43fa47b06612c9dfef5df4ee83f70e26751ef802
camsq <camsq@10.3.62.91>
2025-11-26 12:28:57 +0100

    TP

---
+++ Tp_Quiz_QUILES/src/Quetsion.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author camsq
+ */
+public class Quetsion {
+    private String intitule; 
+    private String proposition1; 
+    private String proposition2; 
+    private String proposition3; 
+    private String proposition4; 
+    private int indexBonneReponse; 
+    
+}
